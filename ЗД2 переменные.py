200f29fd3c7024fd1bd9e5d60b3e0c3152fb3870
NetaliRA <natalianak4@gmail.com>
2024-09-24 01:34:08 +0300

    Add files via upload

---
+++ ЗД2 переменные.py
@@ -0,0 +1,5 @@
+done_hwork = "12"
+used_time = "1,5"
+curse_name = "Python"
+used_time_1_hwork = 1.5 / 12
+print(curse_name, ',всего задач', ':',  done_hwork, ',затрачено часов', ':', used_time, ',среднее время выполнения', used_time_1_hwork)
