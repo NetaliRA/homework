65ccd2b6efcb4ead75e530579c4b484940ce18e8
NetaliRA <natalianak4@gmail.com>
2024-09-28 16:13:39 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = tuple_ = 1, 2, 3, 'a', 'b'
+print(tuple_)
+# tuple_ [0] = 5 невозможно кортеж не изменяемый
+mutable_list = [1, 2, 3, 'a', 'b']
+print(mutable_list)
+mutable_list [3] = 'appel pie'
+print(mutable_list)
