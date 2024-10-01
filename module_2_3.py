2848266e807131b6adcb8676664975f2d9f57a7a
NetaliRA <natalianak4@gmail.com>
2024-10-02 02:11:07 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = 0
-while a < len(my_list):
-    num = my_list[a]
-    a = a + 1
+counter = 0
+while counter < len(my_list):
+    num = my_list[counter]
+    counter = counter + 1
     if num == 0:
         continue
     elif num < 0:
