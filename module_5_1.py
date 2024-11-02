9b5c4dcd00a2731451e5f6f06f950f0d03136891
Natalia <natalianak4@gmail.com>
2024-11-03 01:21:52 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,20 @@
+class House:
+    def __init__(self, name, number_of_floors):
+       self.name = name
+       self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        floor = 0
+        print(f'Здание {self.name} имеет {self.number_of_floors} этажей \nПоднимаемся на {new_floor}-й')
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print(f'{new_floor} - такого этажа не существует')
+        else:
+            for floor in range(new_floor):
+                print(floor + 1)
+
+
+topfloor = House('Последний этаж', 13)
+parking = House('Парковка', 1)
+
+topfloor.go_to(9)
+parking.go_to(-1)
